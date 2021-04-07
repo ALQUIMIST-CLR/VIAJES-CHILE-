1d5cf62d1b9521fae8a9998073c28e4276a8e64e
@ALQUIMIST-CLR <carolynelespay@gmail.com>
2021-04-06 22:30:00 -0400

    Se han incorporado el codigo de js para activar carusel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Código método polyfill para smoothscroll del navegador y secciones
 let buttons = document.querySelectorAll('a');
   buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -9,3 +10,25 @@ let buttons = document.querySelectorAll('a');
       });
     });
   });
+  // Código para inicio de carusel con Javascript
+$(document).ready(function(){
+      // Activate Carousel
+   $("#carouselExampleControls").carousel();
+      // Enable Carousel Indicators
+   $(".carousel-item active").click(function(){
+       $("#carouselExampleControls").carousel(0);
+   });
+   $(".carousel-item 1").click(function(){
+       $("#carouselExampleControls").carousel(1);
+   });
+   $(".carousel-item 2").click(function(){
+       $("#carouselExampleControls").carousel(2);
+   });
+      // Enable Carousel Controls
+   $(".carousel-control-prev").click(function(){
+     $("#carouselExampleControls").carousel("prev");
+  });
+   $(".carousel-control-next").click(function(){
+     $("#carouselExampleControls").carousel("next");
+   });
+});
